79ab37a0c8de2707d05423f1238ed7e3ae59cb0a
Juliana Maciel Cariri dos Santos <julyanamcs@gmail.com>
2021-01-09 00:15:38 -0300

    imprimindo os valosres do banco de produtos[C

--- src/app/Program.java
+++ src/app/Program.java
@@ -6,11 +6,30 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import db.DB;
+import entities.Product;
 
 public class Program {
 
 	public static void main(String[] args) throws SQLException {
 		
+		Product product = new Product();
+		
+		product.setId(1L);
+		
+		product.setName("Pizza");
+		
+		product.setPrice(35.60); 
+		
+		product.setDescription("pIZZA COM  MUITO QUEIJO E MASSA CROCANTE"); 
+		
+		product.setImageUri("https://github.com/devsuperior/1.png");
+		
+		System.out.println("Id: " + product.getId()  + "\r\n" + "Nome do produto: " + product.getName() + "\r\n" +
+				"Price: " + product.getPrice() + "\r\n" + "Description: " + product.getDescription() + "\r\n" +
+				"Image: " + product.getImageUri());
+		
+		
+		/**
 		Connection conn = DB.getConnection();
 	
 		Statement st = conn.createStatement();
@@ -19,6 +38,6 @@ public class Program {
 			
 		while (rs.next()) {
 			System.out.println(rs.getLong("Id") + ", " + rs.getString("Name"));
-		}
+		}*/
 	}
 }
---
+++ src/entities/Product.java
@@ -0,0 +1,119 @@
+/**
+ * 
+ */
+package entities;
+
+/**
+ * @author julya
+ *
+ */
+public class Product {
+	
+	private Long id; 
+	
+	private String name; 
+	
+	private Double price;
+	
+	private String description;
+	
+	private String imageUri;
+
+	
+	
+	/***/
+	public Product() {
+	
+	}
+
+	
+	/**
+	 * @param id
+	 * @param name
+	 * @param price
+	 * @param description
+	 * @param imageUri
+	 */
+	public Product(Long id, String name, Double price, String description, String imageUri) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.description = description;
+		this.imageUri = imageUri;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the price
+	 */
+	public Double getPrice() {
+		return price;
+	}
+
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the imageUri
+	 */
+	public String getImageUri() {
+		return imageUri;
+	}
+
+	/**
+	 * @param imageUri the imageUri to set
+	 */
+	public void setImageUri(String imageUri) {
+		this.imageUri = imageUri;
+	} 
+	
+	
+	
+
+}
